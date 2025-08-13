7d2edb3ae775e7e53a6752edd8df709a5d82d40c
SANTONXD <santiago1210molina@gmail.com>
2025-08-13 11:44:48 -0500

    Delete assets directory

--- assets/js/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleBtn = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector(".main-nav");
-
-    toggleBtn.addEventListener("click", () => {
-        navMenu.classList.toggle("active");
-    });
-});
